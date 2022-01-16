6e5c32a18ffb1a089d284309eeac4c0abad338e3
atrujillofalcon <atrujillo92work@gmail.com>
2022-01-16 19:08:45 +0100

    Init commit and setting mongodb

---
+++ src/main/kotlin/es/atrujillo/sample/funwalletms/FunWalletMsApplication.kt
@@ -0,0 +1,11 @@
+package es.atrujillo.sample.funwalletms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunWalletMsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FunWalletMsApplication>(*args)
+}
---
+++ src/test/kotlin/es/atrujillo/sample/funwalletms/FunWalletMsApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.atrujillo.sample.funwalletms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FunWalletMsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
